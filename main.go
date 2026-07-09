f62bcc3676590fd15c52c4371e85b6a7bb5768b4
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-09 11:43:02 +0700

    feat: display query  info

--- main.go
+++ main.go
@@ -23,19 +23,25 @@ func main() {
 		utils.Cls()
 
 		if len(query) != 0 {
+			count := 0
+
 			for _, character := range data.Results {
 				if !strings.Contains(strings.ToLower(character.Name), query) {
 					continue
 				}
+				count++
+
 				fmt.Printf("Id: %v\n", character.Id)
 				fmt.Printf("Name: %s\n", character.Name)
 				fmt.Printf("Status: %s\n", character.Status)
 				fmt.Printf("Species: %s\n", character.Species)
 				fmt.Println()
 			}
+
+			fmt.Printf("Found %v result(s) for: %s", count, query)
+			fmt.Println()
 		}
 
-		fmt.Println()
 		fmt.Print("Masukkan nama atau 0 untuk keluar: ")
 		scanner.Scan()
 
