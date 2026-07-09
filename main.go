756cfd9cf12c275c3a7c372d4e1d35c60e271061
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-09 11:04:46 +0700

    feat: output characters with newlines

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 
 	"github.com/ryansuhartanto/koda-b8-golang7/req"
+	"github.com/ryansuhartanto/koda-b8-golang7/utils"
 )
 
 func main() {
+	utils.Cls()
 	data := req.Loading(func() *req.CharactersData {
 		return req.Characters()
 	})
 
-	fmt.Println(*data)
+	utils.Cls()
+	for _, character := range data.Results {
+		fmt.Printf("%s\n", character.Name)
+	}
 }
--- req/loading.go
+++ req/loading.go
@@ -3,12 +3,9 @@ package req
 import (
 	"fmt"
 	"sync"
-
-	"github.com/ryansuhartanto/koda-b8-golang7/utils"
 )
 
 func Loading[T any](req func() T) (data T) {
-	utils.Cls()
 	fmt.Println("Loading...")
 
 	var wg sync.WaitGroup
